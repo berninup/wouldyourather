352f085cdbdc00744c23972bd066450cf83df98d
berninup <chris.basham@live.com>
2022-02-20 15:37:50 -0600

    created signup

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,29 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+
+
+
+
+
+def signup(request):
+
+    error_message = ''
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')
+        else:
+            error_message = 'invalid credentials - please try again'
+
+    form = UserCreationForm
+    context = {'form': form, 'error': error_message}
+    return render(request, 'registration/signup.html', context)
 
 def home(request):
-    return HttpResponse('sup bitch')
+    return render(request, 'home.html')
+
+
