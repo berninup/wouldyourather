bfcf049bce640546ce0dad120a0358be910880cb
berninup <chris.basham@live.com>
2022-02-22 14:21:25 -0600

    Created login, created models

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from main_app.models import Choice, WouldYouRather
+
 # Register your models here.
+admin.site.register(WouldYouRather)
+admin.site.register(Choice)
--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class WouldYouRather(models.Model):
+    question = models.CharField(max_length=250)
+
+class Choice(models.Model):
+    choice = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+    question = models.ForeignKey(WouldYouRather, on_delete=models.CASCADE)
